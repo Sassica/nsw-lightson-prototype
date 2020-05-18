2bc7d77937ab65163eb0fd011b86b87705a89aee
Jess <jess@Jesss-MacBook-Pro.local>
2020-05-18 21:11:38 +1000

    add clean-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,13 @@
-const path = require('path');
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
   entry: './src/_assets/js/index.js',
   output: {
     path: path.resolve(__dirname, 'build/assets'),
     filename: 'js/dist.js'
-  }
+  },
+  plugins: [
+	  new CleanWebpackPlugin()
+	]
 }
